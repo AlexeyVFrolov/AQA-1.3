4dbd29f6277d4db526ee29846100185ba23385d3
AlexeyVFrolov <isbase@mail.ru>
2021-05-20 21:24:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/0ogl9f4dqap0scai?svg=true)](https://ci.appveyor.com/project/AlexeyVFrolov/aqa-1-3)
---
+++ src/test/java/AppOrderTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "D:\\_DATA\\Desktop\\ATestProjects\\1.3\\chromedriver\\linux\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "D:\\_DATA\\Desktop\\ATestProjects\\1.3\\chromedriver\\win\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver =null;
+    }
+
+    @Test
+    void shouldSubmitRequest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василиса");
+
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79270000000");
+
+        driver.findElement(By.className("checkbox__box")).click();
+
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
